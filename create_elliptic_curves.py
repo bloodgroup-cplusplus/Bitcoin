49c608c4dd645758fca8906da2710f5e7b35b971
Bhushan <tucater10896601@protonmail.com>
2022-03-25 21:25:55 +0530

    added __init__ and __eq__ dunders in the class Point

---
+++ create_elliptic_curves.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self,x,y,a,b):
+        self.a=a
+        self.b=b
+        self.x =x 
+        self.y = y
+
+        if self.y**2 ! = self.x**3 +a*x+b:
+            raise ValueError(f'{x},{y} is not in the curve')
+
+
+    def __eq__(self,other):
+        return self.x == other.x and self.y == other.y  and self.a == other.a and self.b == other.b
+
+
+
